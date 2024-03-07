9cfb94079fb611866cc644448c8bcf0a075cb730
PepperLola <PepperLola@users.noreply.github.com>
2024-03-06 16:05:41 -0800

    feat: added player data from another endpoint

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from nba_api.stats.endpoints import playercareerstats, commonallplayers
+from nba_api.stats.endpoints import commonplayerinfo, playercareerstats, commonallplayers
 import pandas as pd
 import json
 import time
@@ -14,28 +14,32 @@ i = 0
 results = []
 errors = []
 
+
+def parse_results(info, career):
+    d = {}
+    for vals in [info, career]:
+        for result in vals['resultSets']:
+            for (idx, header) in enumerate(result['headers']):
+                rows = result['rowSet']
+                if len(rows) > 0:
+                    d[header] = rows[-1][idx]
+            break
+    return d
+
 def get_player_career(id: int):
     global i, result
     print(f"Fetching {i}/{n} - {id}.....       ", end="\r")
     i += 1
     time.sleep(0.5)
     try:
-        vals = playercareerstats.PlayerCareerStats(player_id=id).get_dict()
-        d = {}
-        for result in vals['resultSets']:
-            if result['name'] == "SeasonTotalsRegularSeason":
-                for (idx, header) in enumerate(result['headers']):
-                    rows = result['rowSet']
-                    if len(rows) > 0:
-                        d[header] = rows[-1][idx]
-                break
-        results.append(d)
+        career = playercareerstats.PlayerCareerStats(player_id=id).get_dict()
+        info = commonplayerinfo.CommonPlayerInfo(player_id=id).get_dict()
+        results.append(parse_results(info, career))
     except:
         errors.append(id)
     return id
 
-stats = ids[:].map(get_player_career)
-# print(results)
+stats = ids[:50].map(get_player_career)
 df = pd.DataFrame(results)
 df.to_csv("stats.csv")
 with open("errors.json", "w") as f:
